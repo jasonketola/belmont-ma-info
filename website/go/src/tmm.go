d80ed2b815deec21d067cb52569160081fc83e4a
jasonketola <jasonketola@gmail.com>
2021-03-09 03:33:15 +0000

    wip

--- website/go/src/tmm.go
+++ website/go/src/tmm.go
@@ -2,4 +2,36 @@ package main
 
 import (
 	"fmt"
-	
+	"net/http"
+
+	//	"html/template"
+	"database/sql"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	name, err := os.Getwd()
+	checkErr(err)
+	fmt.Fprintf(w, "HOSTNAME : %s\n", name)
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "../../sqlite/tmm.db")
+	checkErr(err)
+
+	rows, err := db.Query("SELECT * FROM years_served")
+	checkErr(err)
+
+	fmt.Println(rows)
+
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
