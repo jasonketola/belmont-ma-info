65249c543f46a00943fe7387f80311a0cabe882d
jasonketola <jasonketola@gmail.com>
2021-03-10 02:35:09 +0000

    Playing with sqlite.

--- website/go/src/tmm.go
+++ website/go/src/tmm.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	//	"html/template"
+	"html/template"
 	"database/sql"
 	"os"
-
+	"log"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,16 +17,43 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HOSTNAME : %s\n", name)
 }
 
-func main() {
+
+func firstNames(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
 	db, err := sql.Open("sqlite3", "../../sqlite/tmm.db")
 	checkErr(err)
 
-	rows, err := db.Query("SELECT * FROM years_served")
-	checkErr(err)
+	rows, err := db.Query("SELECT * FROM years_served where \"Seeking Re-election 2021.04.06\" = 'TRUE'")
 
-	fmt.Println(rows)
+	var precinct, first, middle, last, streetnum, streetname, unitnum, yearsserved, reelection string
+	
+	var data []string
+	
+
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&precinct, &first, &middle, &last, &streetnum, &streetname, &unitnum, &yearsserved, &reelection)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(precinct, first, middle, last, streetnum, streetname, unitnum, yearsserved, reelection)
+		data = append(data, first)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+        tmpl.Execute(w, data)
+    
+}
+
+func main() {
+
 
 	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/firstnames", firstNames)
 	http.ListenAndServe(":8080", nil)
 }
 
